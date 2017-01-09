034367796b3f6ddb2e8f0d33e6a440603ee43a51
YourtionGuo <yourtion@gmail.com>
2017-01-09 22:08:29 +0800

    add iOS demo

---
+++ Samples/RLDemo-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RLDemo-iOS
+//
+//  Created by YourtionGuo on 09/01/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Samples/RLDemo-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RLDemo-iOS
+//
+//  Created by YourtionGuo on 09/01/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
