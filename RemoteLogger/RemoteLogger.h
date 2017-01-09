de6d3930530fd33f9fa3f8b59c29222ca83edca2
YourtionGuo <yourtion@gmail.com>
2017-01-09 22:05:36 +0800

    Init lib

---
+++ RemoteLogger/RemoteLogger.h
@@ -0,0 +1,19 @@
+//
+//  RemoteLogger.h
+//  RemoteLogger
+//
+//  Created by YourtionGuo on 09/01/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RemoteLogger.
+FOUNDATION_EXPORT double RemoteLoggerVersionNumber;
+
+//! Project version string for RemoteLogger.
+FOUNDATION_EXPORT const unsigned char RemoteLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteLogger/PublicHeader.h>
+
+
---
+++ RemoteLoggerTests/RemoteLoggerTests.m
@@ -0,0 +1,39 @@
+//
+//  RemoteLoggerTests.m
+//  RemoteLoggerTests
+//
+//  Created by YourtionGuo on 09/01/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RemoteLoggerTests : XCTestCase
+
+@end
+
+@implementation RemoteLoggerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
